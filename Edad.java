4516e5921c27d0cc795d5e634e883ba1dd36a309
Selegna01 <20243d008@utez.edu.mx>
2025-05-12 13:22:26 -0600

    Codigo de pedir su edad al usuario

---
+++ Edad.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class dad{
+    private static final Scanner scanner = new Scanner (System.in);
+    public static int obtenerNumero(String mensaje){
+        int numeroTemporal;
+        System.out.println(mensaje);
+        numeroTemporal = scanner.nextInt();
+        return numeroTemporal;
+    }
+    public static void main(String [] args ){
+        int edad= obtenerNumero("Ingresa tu edad");
+        if(edad>=18){
+             System.out.println("Eres mayor de edad.");
+        }else
+        {
+             System.out.println("Ere menor de edad");
+        }
+    }
+}
